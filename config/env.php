acb1a1c5f14982e9375cf2a6172ef1b44dc116d3
mantvmass <kliop2317@gmail.com>
2023-07-17 12:51:11 +0700

    fix bug for package name

--- config/env.php
+++ config/env.php
@@ -1,6 +1,6 @@
 <?php
 
-    namespace Config\Env;
+    namespace Config;
 
     class ENV {
 
--- public/index.php
+++ public/index.php
@@ -5,9 +5,6 @@
     date_default_timezone_set("Asia/Bangkok");
     error_reporting(E_ALL); // show all error  | E_ALL = Show all / 0 off show
 
-    // Load Configs ENV
-    require __DIR__.'/../config/env.php';
-
     // Load Composer's autoloader
     require __DIR__.'/../vendor/autoload.php';
 
@@ -24,8 +21,8 @@
     |--------------------------------------------------------------------------
     */
 
-    use Illuminati\Auduct as app;
-    use Illuminati\Http\Request as request;
+    use Soyer\PMSoyer as app;
+    use Soyer\Http\Request as request;
 
     request::handleRequest();
     app::listen(request::$path, request::$method);
